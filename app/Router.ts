ca847bac87b9af260d0fedd2f5f06792fb764457
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-04-21 20:06:18 -0300

    Revert "Acciones del controlador y middlewares"

--- app/Router.ts
+++ app/Router.ts
@@ -1,30 +1,23 @@
 import * as express from 'express'
-import { AnyMiddleware } from './middlewares/AnyMiddleware'
-import { AnyRoute } from './routes/anyRoute'
+import * as homeRoutes from './routes/homeRoute'
 
 interface IRoute {
     path: string,
-    middleware: any[]
     handler: express.Router
 }
 
 export class Routes {
 
     public routes: IRoute[]
-    public anyRoute: AnyRoute
-    public anyMiddleware: AnyMiddleware
 
     constructor() {
-        this.anyRoute = new AnyRoute()
-        this.anyMiddleware = new AnyMiddleware()
         this.loadRoutes()
     }
 
     private loadRoutes(): void {
         this.routes = [{
-            path: "/any",
-            middleware: [this.anyMiddleware.anyCheck, this.anyMiddleware.anyCheckTwo],
-            handler: this.anyRoute.router,
+            path: "/home",
+            handler: homeRoutes.default,
         }]
     }
 
