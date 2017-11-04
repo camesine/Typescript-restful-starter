70793ff26be84b18949346cd3fbcae34f15187c8
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-11-04 20:43:03 -0300

    Acciones controlador sample con typeorm

--- dist/tasks/migrate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Connection_1 = require("../config/db/Connection");
-Connection_1.Connection.then(conn => {
-    conn.migrations;
-});
-//# sourceMappingURL=migrate.js.map
