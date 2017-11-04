d4de736f3d310055122a61ee2704b6b27183fc01
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-11-03 22:18:00 -0300

    Archivo de conexion Typeorm

--- config/db/Connection.ts
+++ config/db/Connection.ts
@@ -1,5 +1,5 @@
-import * as Sequelize from 'sequelize'
 import { config } from '../../config'
+import { createConnection } from 'typeorm'
 
 const SERVER = process.env.SERVER || config.DATABASE.SERVER || 'localhost'
 const DATABASE = process.env.DB || config.DATABASE.DB || "test"
@@ -7,7 +7,16 @@ const USER = process.env.USER || config.DATABASE.USER || 'root'
 const PASSWORD = process.env.PASSWORD || config.DATABASE.PASSWORD || ''
 const DIALECT = process.env.DIALECT || config.DATABASE.DIALECT || 'mariadb'
 
-export const db = new Sequelize(DATABASE, USER, PASSWORD,{
-        host: SERVER,
-        dialect: DIALECT,
-    })
+export const Connection = createConnection({
+    type: DIALECT,
+    host: SERVER,
+    port: 3306,
+    username: USER,
+    password: PASSWORD,
+    database: DATABASE,
+    entities: [
+        
+    ],
+    synchronize: true,
+    logging: false
+})
