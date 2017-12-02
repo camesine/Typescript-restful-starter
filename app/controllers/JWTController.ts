ed75141009ca8b10f5f5f21cbad8df1d0ad89054
Hector <hector.riquelme1169@gmail.com>
2017-12-01 21:10:08 -0300

    refactor JWTController

--- app/controllers/JWTController.ts
+++ app/controllers/JWTController.ts
@@ -3,10 +3,10 @@ import { JWTService } from '../services/JWTService'
 
 export class JWTController {
 
-    public index = async (req: express.Request, res: express.Response) => {
+    public static async Index (req: express.Request, res: express.Response) {
         const payload = req.body.payload
         const token = await JWTService.signToken(payload)
-        res.send(token)
+        return res.send(token)
     }
 
 }
