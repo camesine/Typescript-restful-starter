d4d22f2f666bfe93606b6cc12306a8ea30bccc15
Hector <hector.riquelme1169@gmail.com>
2017-12-04 11:08:01 -0300

    rename variable

--- app/repository/SampleRepository.ts
+++ app/repository/SampleRepository.ts
@@ -4,8 +4,8 @@ import { Sample } from '../entity/Sample'
 @EntityRepository(Sample)
 export class SampleRepository extends Repository<Sample> {
 
-    public BukCreate(_Samples : Sample[]): Promise<any> {
-        return this.manager.createQueryBuilder().insert().into(Sample).values(_Samples).execute()
+    public BukCreate(Samples: Sample[]): Promise<any> {
+        return this.manager.createQueryBuilder().insert().into(Sample).values(Samples).execute()
     }
 
 }
