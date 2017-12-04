225e718e17d67e94254a8e8a22651b20a0870ccf
Hector <hector.riquelme1169@gmail.com>
2017-12-04 11:08:30 -0300

    rename variable

--- app/services/SampleService.ts
+++ app/services/SampleService.ts
@@ -5,17 +5,17 @@ import { getCustomRepository } from 'typeorm'
 export class SampleService {
 
     private SampleRepository: SampleRepository
-    
+
     constructor() {
         this.SampleRepository = getCustomRepository(SampleRepository)
     }
 
-    public FindByText = (sample : Sample) : Promise<Sample[]> => {
+    public FindByText = (sample: Sample): Promise<Sample[]> => {
         return Sample.FindByText(sample)
     }
 
-    public BulkCreate = (_Samples : Sample[]) : Promise<Sample[]> => {
-        return this.SampleRepository.BukCreate(_Samples)
+    public BulkCreate = (Samples: Sample[]): Promise<Sample[]> => {
+        return this.SampleRepository.BukCreate(Samples)
     }
 
 }
