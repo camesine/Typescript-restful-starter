f7a8ae0ad02b377b465f7430199a3e9612375060
Hector <hector.riquelme1169@gmail.com>
2018-01-28 15:54:02 -0300

    Remove process.env

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
+import { env } from 'process'
+
 const LOCAL_CONFIGURATION = {
     SERVER: "127.0.0.1",
     PORT_DB: 3306,
@@ -8,16 +10,16 @@ const LOCAL_CONFIGURATION = {
 }
 
 const PRODUCTION_CONFIGURATION = {
-    SERVER: process.env.SERVER || 'localhost',
-    DB: process.env.DB || "prod",
-    PORT_DB: process.env.PORT_DB || 3306,
-    USER_DB: process.env.USER_DB || 'root',
-    PASSWORD: process.env.PASSWORD || '',
-    DIALECT: process.env.DIALECT || 'mysql',
+    SERVER: env.SERVER || 'localhost',
+    DB: env.DB || "prod",
+    PORT_DB: env.PORT_DB || 3306,
+    USER_DB: env.USER_DB || 'root',
+    PASSWORD: env.PASSWORD || '',
+    DIALECT: env.DIALECT || 'mysql',
 }
 
 export const config = {
     SECRET: "HltH3R3",
     PORT_APP: 1344,
-    DATABASE: process.env.NODE_ENV === 'PRODUCTION' ? PRODUCTION_CONFIGURATION : LOCAL_CONFIGURATION
+    DATABASE: env.NODE_ENV === 'PRODUCTION' ? PRODUCTION_CONFIGURATION : LOCAL_CONFIGURATION
 }
