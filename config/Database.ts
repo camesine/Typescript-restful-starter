36fc493d9c3931aa14b38b5ad739a0891c43dc6c
Hector <hriquelme@artool.cl>
2018-07-11 23:50:13 -0400

    Align indent

--- config/Database.ts
+++ config/Database.ts
@@ -3,15 +3,15 @@ import { Sample } from "../app/models/Sample.model";
 import { config, DIALECT } from "../config";
 
 export const Connection = createConnection({
-  database: config.DATABASE.DB,
-  entities: [
-    Sample,
-  ],
-  host: config.DATABASE.SERVER,
-  logging: false,
-  password: config.DATABASE.PASSWORD,
-  port: config.DATABASE.PORT_DB,
-  synchronize: true,
-  type: DIALECT,
-  username: config.DATABASE.USER_DB,
+    database: config.DATABASE.DB,
+    entities: [
+        Sample,
+    ],
+    host: config.DATABASE.SERVER,
+    logging: false,
+    password: config.DATABASE.PASSWORD,
+    port: config.DATABASE.PORT_DB,
+    synchronize: true,
+    type: DIALECT,
+    username: config.DATABASE.USER_DB,
 });
