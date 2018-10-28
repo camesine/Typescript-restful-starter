9d9409a735bd380cd990db96b4822ebd1f037c38
Hector <hector.riquelme1169@gmail.com>
2018-10-28 00:12:07 -0300

    refactor constructor sample

--- app/controllers/Sample.controller.ts
+++ app/controllers/Sample.controller.ts
@@ -6,9 +6,11 @@ import { Controller } from "./Controller";
 export class SampleController extends Controller {
 
     private sampleService: SampleService;
+    private sample: Sample;
 
     constructor(req: express.Request, res: express.Response) {
         super(req, res);
+        this.sample = new Sample();
         this.sampleService = new SampleService();
     }
 
@@ -18,18 +20,17 @@ export class SampleController extends Controller {
     }
 
     public async find(): Promise<express.Response> {
-        const { id } = this.req.params;
+        const { id } = this.req.params as { id: number };
         const sample = await this.sampleService.findOneById(id);
         return sample ? this.res.status(200).send(sample) : this.res.status(404).send({ text: "NOT FOUND" });
     }
 
     public async create(): Promise<express.Response> {
-        const { text } = this.req.body;
-        const sample = new Sample();
-        sample.text = text;
-        sample.email = "someone@somewhere.com";
+        const { text } = this.req.body as { text: string };
+        this.sample.text = text;
+        this.sample.email = "someone@somewhere.com";
         try {
-            const result = await this.sampleService.save(sample);
+            const result = await this.sampleService.save(this.sample);
             return this.res.status(200).send(result);
         } catch (ex) {
             return this.res.status(404).send({ text: "ERROR" });
@@ -37,26 +38,25 @@ export class SampleController extends Controller {
     }
 
     public async update(): Promise<express.Response> {
-        const sample = new Sample();
-        const { id, text, email } = this.req.body;
-        sample.id = id;
-        sample.text = text;
-        sample.email = email;
+        const { id, text, email } = this.req.body as { id: number, text: string, email: string };
+        this.sample.id = id;
+        this.sample.text = text;
+        this.sample.email = email;
         try {
-            const result = await this.sampleService.save(sample);
+            const result = await this.sampleService.save(this.sample);
             return result ? this.res.status(200).send() : this.res.status(404).send({ text: "NOT FOUND" });
         } catch (ex) {
-            return this.res.status(404).send({text: "ERROR"});
+            return this.res.status(404).send({ text: "ERROR" });
         }
     }
 
     public async delete(): Promise<express.Response> {
-        const { id } = this.req.body;
+        const { id } = this.req.body as { id: number };
         try {
             await this.sampleService.removeById(id);
             return this.res.status(204).send();
         } catch (ex) {
-            return this.res.status(404).send({text: "ERROR"});
+            return this.res.status(404).send({ text: "ERROR" });
         }
     }
 
